aacd570b017f1c5f05b4afb99807ce77a4523d9a
ikanx101 <ikanx101@gmail.com>
2021-11-02 13:19:51 +0700

    done

--- program.py
+++ program.py
@@ -49,13 +49,19 @@ def level():
   if lvl == 1:
     x1 = 1
     x2 = 30
+    x3 = 1
+    x4 = 5
   if lvl == 2:
     x1 = 30
     x2 = 70
+    x3 = 4
+    x4 = 9
   if lvl == 3:
     x1 = 70
     x2 = 200
-  out = [x1,x2]
+    x3 = 7
+    x4 = 15
+  out = [x1,x2,x3,x4]
   return(out)
 
 # fungsi untuk menentukan n soal
@@ -103,6 +109,46 @@ def kurang(x1,x2) :
      # print(cek)
   return(cek,total_waktu)
 
+# fungsi perkalian
+def kali(x3,x4) :
+  a = randint(x3,x4)
+  b = randint(x3,x4)
+  c = a * b
+  print(a, ' x ', b, ' = ')
+  now = datetime.datetime.now()
+  jawab = input('jawab: ')
+  end = datetime.datetime.now()
+  time_delta = end-now
+  total_waktu = time_delta.total_seconds()
+  jawab = int(jawab)
+  if c == jawab :
+     cek = 1
+     # print(cek)
+  else :
+     cek = 0
+     # print(cek)
+  return(cek,total_waktu)
+
+# fungsi pembagian
+def bagi(x3,x4) :
+  a = randint(x3,x4)
+  b = randint(x3,x4)
+  c = a * b
+  print(c, ' : ', a, ' = ')
+  now = datetime.datetime.now()
+  jawab = input('jawab: ')
+  end = datetime.datetime.now()
+  time_delta = end-now
+  total_waktu = time_delta.total_seconds()
+  jawab = int(jawab)
+  if b == jawab :
+     cek = 1
+     # print(cek)
+  else :
+     cek = 0
+     # print(cek)
+  return(cek,total_waktu)
+
 
 # =========================================
 # tampilannya mulai dari sini
@@ -113,7 +159,10 @@ lv = level()
 # bikin array untuk rekap
 benar = [] # berapa banyak jawaban benar
 waktu = [] # waktu yang dibutuhkan
-tipe_soal = []
+
+# initial condition
+benar[0] = 0
+waktu[0] = 0
 
 # menentukan seberapa banyak soal yang dikerjakan
 print("\n\nPENJUMLAHAN\n")
@@ -125,25 +174,61 @@ print("\n\nPENGURANGAN\n")
 n_pengurangan = n_soal()
 n_pengurangan = int(n_pengurangan) - 1
 
+# menentukan seberapa banyak soal yang dikerjakan
+print("\n\nPERKALIAN\n")
+n_perkalian = n_soal()
+n_perkalian = int(n_perkalian) - 1
+
+# menentukan seberapa banyak soal yang dikerjakan
+print("\n\nPEMBAGIAN\n")
+n_pembagian = n_soal()
+n_pembagian = int(n_pembagian) - 1
+
 clear()
 
 # mulai iterasi untuk penjumlahan
 i = 0
 tes = []
+jumlah_benar = []
 while i <= n_penjumlahan :
   tes = jumlah(lv[0],lv[1])
+  print("\n")
   benar.append(tes[0])
   waktu.append(tes[1])
-  tipe_soal.append("jumlah")
+  jumlah_benar.append(tes[0])
   i = i + 1
 
 # mulai iterasi untuk pengurangan
 i = 0
+kurang_benar = []
 while i<=n_pengurangan :
   tes = kurang(lv[0],lv[1])
+  print("\n")
   benar.append(tes[0])
   waktu.append(tes[1])
-  tipe_soal.append("kurang")
+  kurang_benar.append(tes[0])
+  i = i + 1
+
+# mulai iterasi untuk perkalian
+i = 0
+kali_benar = []
+while i<=n_perkalian :
+  tes = kali(lv[2],lv[3])
+  print("\n")
+  benar.append(tes[0])
+  waktu.append(tes[1])
+  kali_benar.append(tes[0])
+  i = i + 1
+
+# mulai iterasi untuk pembagian
+i = 0
+bagi_benar = []
+while i<=n_pembagian :
+  tes = bagi(lv[2],lv[3])
+  print("\n")
+  benar.append(tes[0])
+  waktu.append(tes[1])
+  bagi_benar.append(tes[0])
   i = i + 1
 
 # output ke user
@@ -151,17 +236,38 @@ print("\n\n")
 print("Kamu berhasil menjawab: ",sum(benar)," soal dari total ",len(benar)," soal")
 print("Waktu yang dibutuhkan: ",round(sum(waktu),4)," detik")
 
+# rekap penjumlahan
+print("\n")
+jumlah_benar = sum(jumlah_benar)
+print("Kamu berhasil menjawab ",jumlah_benar," soal penjumlahan dari ",n_penjumlahan+1," soal")
+
+# rekap pengurangan
+print("\n")
+kurang_benar = sum(kurang_benar)
+print("Kamu berhasil menjawab ",kurang_benar," soal pengurangan dari ",n_pengurangan+1," soal")
+
+# rekap perkalian
+print("\n")
+kali_benar = sum(kali_benar)
+print("Kamu berhasil menjawab ",kali_benar," soal perkalian dari ",n_perkalian+1," soal")
+
+# rekap pembagian
+print("\n")
+bagi_benar = sum(bagi_benar)
+print("Kamu berhasil menjawab ",bagi_benar," soal pembagian dari ",n_pembagian+1," soal")
+print("\n\nGrafik skor sudah tersedia di working directory")
+
 # bikin plot
-benar_cum = np.cumsum(benar)
+benar_cum = np.cumsum(benar)/len(benar) * 100
+benar_cum = np.round(benar_cum,decimals=2)
 waktu_cum = np.cumsum(waktu)
-print(benar_cum)
-print(waktu_cum)
 
 plt.figure(figsize = (16,9))
-plt.plot(waktu_cum,benar_cum,color = "red",linewidth=0.5,linestyle="--",label='Skor')
-plt.xlabel('time')
-plt.ylabel('benar')
+plt.plot(waktu_cum,benar_cum,color = "blue",linewidth=1.5,linestyle="--",label='Skor')
+plt.scatter(waktu_cum,benar_cum,color = "green")
+plt.xlabel('Waktu Menjawab (dalam detik)')
+plt.ylabel('Skor Kumulatif (dalam %)')
 plt.title("Cumulative Score Over Time")
-plt.legend()
+#plt.legend()
 #plt.show()
 plt.savefig('rekap.png',dpi = 450)
