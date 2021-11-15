486b7fb89b439d2c6a692bcd1337fcb02a229b6c
ikanx101 <ikanx101@gmail.com>
2021-11-15 09:12:34 +0700

    new

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-lagi = "y"
+# ========================================
+# program yang akan dieksekusi oleh user
+# kelompok rbl 2
+# 20921003 Winda Wijayasari
+# 20921004 Mohammad Rizka Fadhli
+# ========================================
 
+# jika user baru pertama kali mengeksekusi
+lagi = "y"
 while lagi == "y" or lagi == "Y":
   exec(open("program.py").read())
+  # ditanyakan jika user hendak mencoba kembali atau tidak?
   lagi = input("Coba lagi? (y/n) \n")
 
 print("\n\n")
